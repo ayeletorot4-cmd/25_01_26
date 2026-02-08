5204c031f2ab539a7c412c9d7189802b2a0eb317
AYELET <ayeletorot4@gmail.com>
2026-02-08 17:31:05 +0200

    first trial

--- TARGIL_3.py
+++ TARGIL_3.py
@@ -4,16 +4,17 @@
 #a=2 b=5
 #swich between a and b
 #i.e b=2, a=5
-a=int(input("Enter a number a"))
-b=int(input("Enter a number b"))
+a=int(input("Enter a number a "))
+b=int(input("Enter a number b "))
 
 #c=a
 #a=b
 #b=c
 
-a= a+b
-b=a-b
-a=a-b
+#a= a+b
+#b=a-b
+#a=a-b
+a,b=b,a
 
 
 print("a=",a ,"b=",b)
