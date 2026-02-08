ca69a3e376cbec407ed9959237824a2c09e0af93
AYELET <ayeletorot4@gmail.com>
2026-02-08 17:15:27 +0200

    first trial

---
+++ TARGIL_3.py
@@ -0,0 +1,19 @@
+#targil
+#input 2 int numbers i a,b
+#i.e 2,5
+#a=2 b=5
+#swich between a and b
+#i.e b=2, a=5
+a=int(input("Enter a number a"))
+b=int(input("Enter a number b"))
+
+#c=a
+#a=b
+#b=c
+
+a= a+b
+b=a-b
+a=a-b
+
+
+print("a=",a ,"b=",b)
