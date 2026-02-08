1a7f647e2e523729d7b7a019e06d423f1bc0e7ce
AYELET <ayeletorot4@gmail.com>
2026-02-08 16:49:57 +0200

    first trial

--- TARGIL.py
+++ TARGIL.py
@@ -1,9 +1,11 @@
 #TARGIL
 #input number
 num= int(input("Enter a number"))
-i=1
+
 while num>0:
+    i = 1
     while i<=num:
          print(i)
          i=i+1
     num = int(input("Enter a number"))
+print("finished")
